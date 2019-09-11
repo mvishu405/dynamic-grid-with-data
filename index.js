c189e7520ebd16bdff6595e8a62e5ba3c530a9c4
mvishu405 <mvishu405@gmail.com>
2019-09-11 20:29:32 +0530

    add soritng

--- index.js
+++ index.js
@@ -12,18 +12,27 @@ function people(id, name, title, imageLink, description){
 
 const peopleArray = [
   new people(1, 'Vishal', 'position' , 'https://placehold.it/500x500', 'description'),
-  new people(1, 'Vishal1', 'position' ,'https://placehold.it/500x500', 'description'),
-  new people(1, 'Vishal2', 'position' ,'https://placehold.it/500x500', 'description'),
-  new people(1, 'Vishal2', 'position' ,'https://placehold.it/500x500', 'description'),
-  new people(1, 'Vishal', 'position' , 'https://placehold.it/500x500', 'description'),
-  new people(1, 'Vishal1', 'position' ,'https://placehold.it/500x500', 'description'),
-  new people(1, 'Vishal2', 'position' ,'https://placehold.it/500x500', 'description'),
-  new people(1, 'Vishal2', 'position' ,'https://placehold.it/500x500', 'description'),
-  new people(1, 'Vishal', 'position' , 'https://placehold.it/500x500', 'description'),
-  new people(1, 'Vishal1', 'position' ,'https://placehold.it/500x500', 'description'),
-  new people(1, 'Vishal2', 'position' ,'https://placehold.it/500x500', 'description'),
-  new people(1, 'Vishal2', 'position' ,'https://placehold.it/500x500', 'description'),
+  new people(2, 'Vishal1', 'position' ,'https://placehold.it/500x500', 'description'),
+  new people(3, 'Vishal2', 'position' ,'https://placehold.it/500x500', 'description'),
+  new people(4, 'Vishal2', 'position' ,'https://placehold.it/500x500', 'description'),
+  new people(5, 'Vishal', 'position' , 'https://placehold.it/500x500', 'description'),
+  new people(6, 'Vishal1', 'position' ,'https://placehold.it/500x500', 'description'),
+  new people(7, 'Vishal2', 'position' ,'https://placehold.it/500x500', 'description'),
+  new people(8, 'Vishal2', 'position' ,'https://placehold.it/500x500', 'description'),
+  new people(9, 'Vishal', 'position' , 'https://placehold.it/500x500', 'description'),
+  new people(10, 'Vishal1 sec last', 'position' ,'https://placehold.it/500x500', 'description'),
+  new people(11, 'Vishal2 last', 'position' ,'https://placehold.it/500x500', 'description'),
+  new people(0, 'Vishaluyyuui', 'position' ,'https://placehold.it/500x500', 'description'),
 ];
+
+
+peopleArray.sort((a, b) => {
+  if(a.id < b.id)
+  return 1;
+  if(a.id > b.id)
+  return -1;
+  return 0;
+});
 
 const peopleUI = peopleArray.map((people) => {
   let peopleGrid;
